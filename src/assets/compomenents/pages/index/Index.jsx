5b14613b44f3fba1f473e5f577ea4be9b8b3bb90
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-06-21 16:43:50 +0200

    Creato l'index e home page

--- src/assets/compomenents/pages/index/Index.jsx
+++ src/assets/compomenents/pages/index/Index.jsx
@@ -1,12 +1,60 @@
-import indexStyles from "./index.module.scss"
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import indexStyles from './index.module.scss';
+// import { Link } from "react-router-dom";
 
-const Index = () =>{
-    return(
+const Index = () => {
+    const [posts, setPosts] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Funzione per formattare la data
+    const formatDate = (dateString) => {
+        return dateString.slice(0, 10);
+    }
+
+    // Chiamata per avere i posts
+    useEffect(() => {
+        axios.get('http://localhost:3000/posts')
+            .then(response => {
+                setPosts(response.data.data);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error('Qualcosa è andato storto', error);
+                setIsLoading(false);
+            });
+    }, []);
+
+    return (
         <main>
-            <h1>Index</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 text-center my-5">
+                        <h1 className={indexStyles.blu}><strong>I nostri post</strong></h1>
+                    </div>
+                    {isLoading ? (
+                        <div className="col-12 text-center">
+                            <h1>Caricamento in corso...</h1>
+                        </div>
+                    ) : (
+                        posts.map((post, index) => (
+                            <div key={index} className="col-12 col-md-6 text-center my-3">
+                                <div className="">
+                                    <h2>{post.title}</h2>
+                                    <figure>
+                                        <img src={post.image} alt={post.title} className={indexStyles.image} />
+                                    </figure>
+                                    <p><strong>Creato il:</strong> {formatDate(post.createdAt)}</p>
+                                    <button><h4>Scopri il post</h4></button>
+                                </div>
+
+                            </div>
+                        ))
+                    )}
+                </div>
+            </div>
         </main>
-    )
+    );
 }
 
 export default Index;
